a510288784d50f0b7872538a855b5ea13f4fd0e8
Alba Ruiz de la Vega <ruizdelavegaalba@gmail.com>
2026-03-23 13:18:02 +0100

    Remove comments for armies in Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,19 @@ public class Main {
     public static void main(String[] args) {
         MiddleEarthBattle simulador = new MiddleEarthBattle();
 
-        // --- EJÉRCITO DEL BIEN ---
+       
         Map<String, Integer> ejercitoBien = new HashMap<>();
         ejercitoBien.put("Pelosos", 10);
         ejercitoBien.put("Elfos", 5);
         ejercitoBien.put("Enanos", 2);
 
-        // --- EJÉRCITO DEL MAL ---
+      
         Map<String, Integer> ejercitoMal = new HashMap<>();
         ejercitoMal.put("Orcos", 20);
         ejercitoMal.put("Trolls", 2);
         ejercitoMal.put("Huargos", 5);
 
-        // --- PROCESO DE BATALLA ---
+        
         int puntosBien = simulador.calcularPuntos(ejercitoBien, true);
         int puntosMal = simulador.calcularPuntos(ejercitoMal, false);
 
